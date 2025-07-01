c8d0e72e8950f17333cbdca47d7c734f5b4e2ebf
ChrisCee2 <chrischung411@gmail.com>
2025-07-01 14:37:44 -0400

    GC201-6: Ball bounce direction is based on hit position on paddle

--- GameObjects/paddle.gd
+++ GameObjects/paddle.gd
@@ -3,6 +3,7 @@ class_name Paddle extends StaticBody2D
 @export var isLeft: bool
 @export var characterInput: CharacterInput
 @export var arena: Arena
+@export_range(1, 89) var maxBounceAngle: float = 70
 @onready var controller = $CharacterController
 
 @onready var sprite = $Sprite2D
@@ -30,3 +31,10 @@ func getDistanceFromUpperBound() -> float:
 func getDistanceFromLowerBound() -> float:
 	var paddleSize: Vector2 = getSize()
 	return arena.getLowerBound() - (global_position.y + (paddleSize.y / 2))
+
+func getBounceAngle(bouncePosition: Vector2) -> float:
+	var distanceFromCenter = global_position - bouncePosition
+	if distanceFromCenter.y == 0:
+		return 0
+	# Flipping sign because negative y is up
+	return -(maxBounceAngle * (distanceFromCenter.y / (getSize().y / 2)))
