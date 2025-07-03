212f65f8f8496cc72b72d145953ec1c1339022cd
ChrisCee2 <chrischung411@gmail.com>
2025-07-03 01:12:44 -0400

    GC201-14: Added delay for reaction and input

--- InputMachines/computer_input.gd
+++ InputMachines/computer_input.gd
@@ -5,19 +5,37 @@ class_name ComputerInput extends InputInterface
 @export var ball: Ball
 @export var paddle: Paddle
 
+@onready var reaction_timer: Timer = $ReactionTimer
+@onready var input_timer: Timer = $InputTimer
+
 var rtc_range: Array = [2, 10]
 var should_set_hit_position: bool = false
 var desired_hit_position: float = 0 # Where on paddle to hit the ball
 var should_set_rtc_error: bool = false
 var return_to_center_error: float = 0
-var ball_error: float = 2 # How many pixels it is ok for the paddle to be from the ball
+var ball_error: float = 0 # How many pixels it is ok for the paddle to be from the ball
+var min_move_amount: float = 4 # Minimum amount of pixels needed before moving paddle
+var should_move: bool = false
+var reacted: bool = false
+
+var reaction_speed = 0.26 # Reaction speed in seconds
+var delay_between_inputs: float = 0.1 # Time between inputs in seconds
+var reaction_timer_ended: bool = true
+var input_timer_ended: bool = true
+
+func _ready() -> void:
+	input_timer.timeout.connect(_input_timer_ended)
+	reaction_timer.timeout.connect(_reaction_timer_ended)
+	ball.bounce.connect(reset_reaction_timer)
 
 func update() -> void:
+	if not reaction_timer_ended or not input_timer_ended:
+		return
+	
 	if is_ball_coming() and should_set_hit_position:
 		var hit_range: Array = get_hit_range()
 		desired_hit_position = randf_range(hit_range[0], hit_range[1])
 		should_set_hit_position = false
-		print(desired_hit_position)
 	
 	if not is_ball_coming() and should_set_rtc_error:
 		return_to_center_error = randf_range(rtc_range[0], rtc_range[1])
@@ -25,6 +43,9 @@ func update() -> void:
 	
 	var target = ball.global_position.y + desired_hit_position
 	var distance_from_target: float = paddle.global_position.y - target
+	
+	if (abs(distance_from_target) >= min_move_amount):
+		should_move = true
 	
 	# If ball is not coming, return to center
 	if not is_ball_coming():
@@ -34,13 +55,17 @@ func update() -> void:
 	if is_ball_coming():
 		should_set_rtc_error = true
 	
-	set_direction_to_target(distance_from_target)
+	if should_move:
+		set_direction_to_target(distance_from_target)
 
 func set_direction_to_target(distance_from_target: int) -> void:
 	var error = ball_error if is_ball_coming() else return_to_center_error
 	if abs(distance_from_target) <= error:
 		isPressingUp = false
 		isPressingDown = false
+		should_move = false
+		should_set_hit_position = true
+		reset_input_timer()
 	elif distance_from_target < 0 :
 		isPressingUp = false
 		isPressingDown = true
@@ -56,3 +81,19 @@ func is_ball_coming() -> bool:
 	# Find side of paddle
 	var is_left = true if arena.global_position.x - paddle.global_position.x >= 0 else false
 	return (is_left and ball.velocity.x <= 0) or (not is_left and ball.velocity.x > 0)
+
+func reset_input_timer() -> void:
+	input_timer_ended = false
+	input_timer.stop()
+	input_timer.start(delay_between_inputs)
+
+func reset_reaction_timer() -> void:
+	reaction_timer_ended = false
+	reaction_timer.stop()
+	reaction_timer.start(reaction_speed)
+
+func _input_timer_ended() -> void:
+	input_timer_ended = true
+
+func _reaction_timer_ended() -> void:
+	reaction_timer_ended = true
